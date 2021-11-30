5bbe0a88bd92199427220b48e12a4ca5e27dc4a8
Jorge <jorgecabiedes99@hotmail.com>
2021-11-29 18:22:10 -0600

    Cars now randomly appear every 5 steps

---
+++ Mesa_Models/trafficBase/model.py
@@ -0,0 +1,76 @@
+from mesa import Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+from agent import Road, Traffic_Light, Obstacle, Destination, Car
+import json
+
+
+class RandomModel(Model):
+    """
+    Creates a new model with random agents.
+    Args:
+        N: Number of agents in the simulation
+        height, width: The size of the grid to model
+    """
+
+    def __init__(self, N):
+
+        dataDictionary = json.load(open("mapDictionary.txt"))
+        self.carId = 0
+
+        with open('base.txt') as baseFile:
+            lines = baseFile.readlines()
+            self.width = len(lines[0])-1
+            self.height = len(lines)
+
+            self.grid = MultiGrid(self.width, self.height, torus=False)
+            self.schedule = RandomActivation(self)
+
+            for r, row in enumerate(lines):
+                for c, col in enumerate(row):
+                    if col in ["v", "^", ">", "<"]:
+                        agent = Road(f"r{r*self.width+c}",
+                                     self, dataDictionary[col])
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+                        self.schedule.add(agent)
+                    elif col in ["S", "s"]:
+                        agent = Traffic_Light(
+                            f"tl{r*self.width+c}",
+                            self,
+                            False if col == "S" else True,
+                            int(dataDictionary[col]))
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+                        self.schedule.add(agent)
+                    elif col == "#":
+                        agent = Obstacle(f"ob{r*self.width+c}", self)
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+                    elif col == "D":
+                        agent = Destination(f"d{r*self.width+c}", self)
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
+
+        self.num_agents = N
+        self.running = True
+
+    def step(self):
+        '''Advance the model by one step.'''
+        self.schedule.step()
+        if self.schedule.steps % 10 == 0:
+            for agents, x, y in self.grid.coord_iter():
+                for agent in agents:
+                    if isinstance(agent, Traffic_Light):
+                        agent.state = not agent.state
+
+        if self.schedule.steps % 5 == 0:
+            new_car = Car(self.carId + 1000, self)
+            rAgent = self.random.choice(self.schedule.agents)
+
+            def findPosition(rAgent):
+                print(rAgent)
+                if isinstance(rAgent, Road):
+                    self.grid.place_agent(new_car, rAgent.pos)
+                else:
+                    rAgent = new_car.random.choice(self.schedule.agents)
+                    findPosition(rAgent)
+
+            findPosition(rAgent)
+
